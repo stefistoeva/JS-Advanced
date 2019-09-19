6f6fdc8bbf5bc110a30cd880ad602fc9fcab3813
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-09-19 09:53:58 +0300

    Update 07.Cooking by numbers.js

--- 01.Syntax, Functions and Statements/07.Cooking by numbers.js
+++ 01.Syntax, Functions and Statements/07.Cooking by numbers.js
@@ -1,19 +1,31 @@
 function solve(array) {
-    let start = Number(array[0]);
-    for(let i = 1; i <= 5; i++) {
-        switch(array[i]) {
-            case 'chop': console.log(start /= 2);
-            break;
-            case 'dice': console.log(start = Math.sqrt(start));
-            break;
-            case 'spice': console.log(++start);
-            break;
-            case 'bake': console.log(start *= 3);
-            break;
-            case 'fillet': console.log(start -= start * 0.20);
-            break;
-            default: break;
-        }
+    let start = Number(array.shift());
+
+    let operations = {
+        chop: (num) => {return (num / 2)},
+        dice: (num) => {return (Math.sqrt(num))},
+        spice: (num) => {return ++num},
+        bake: (num) => {return num *= 3},
+        fillet: (num) => {return num -= num * 0.20}
+    }
+
+    for(let i = 0; i < 5; i++) {
+       start = operations[array[i]](start);
+       console.log(start);
+
+        // switch(array[i]) {
+        //     case 'chop': console.log(start /= 2);
+        //     break;
+        //     case 'dice': console.log(start = Math.sqrt(start));
+        //     break;
+        //     case 'spice': console.log(++start);
+        //     break;
+        //     case 'bake': console.log(start *= 3);
+        //     break;
+        //     case 'fillet': console.log(start -= start * 0.20);
+        //     break;
+        //     default: break;
+        // }
     }
 }
 
