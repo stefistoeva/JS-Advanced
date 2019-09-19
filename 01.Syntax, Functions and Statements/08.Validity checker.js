6eb5c5f07981a089b0f65ef26842649643644adb
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-09-19 10:17:30 +0300

    Create 08.Validity checker.js

---
+++ 01.Syntax, Functions and Statements/08.Validity checker.js
@@ -0,0 +1,30 @@
+function solve(array) {
+    let x1 = Number(array.shift());
+    let y1 = Number(array.shift());
+    let x2 = Number(array.shift());
+    let y2 = Number(array.shift());
+
+    function distance(x1, y1, x2, y2) {
+        return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2);
+    }
+
+    if (Number.isInteger(distance(x1, y1, 0, 0))) {
+        console.log(`{${x1}, ${y1}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x2, y2, 0, 0))) {
+        console.log(`{${x2}, ${y2}} to {0, 0} is valid`);
+    } else {
+        console.log(`{${x2}, ${y2}} to {0, 0} is invalid`);
+    }
+
+    if (Number.isInteger(distance(x1, y1, x2, y2))) {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is valid`);
+    } else {
+        console.log(`{${x1}, ${y1}} to {${x2}, ${y2}} is invalid`);
+    }
+}
+
+solve([3, 0, 0, 4]);
