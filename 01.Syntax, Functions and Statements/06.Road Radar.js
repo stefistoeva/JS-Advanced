677e30d8b21565bf6088d6701681a71fa685f578
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-09-24 09:42:41 +0300

    Update 06.Road Radar.js

--- 01.Syntax, Functions and Statements/06.Road Radar.js
+++ 01.Syntax, Functions and Statements/06.Road Radar.js
@@ -1,24 +1,18 @@
 function solve(array) {
-    let speed = Number(array[0]);
-    let area = String(array[1]);
+    let [speed, area] = array;
 
-    let limit = 0;
-    switch(area){
-        case 'motorway': limit = 130;
-            break;
-        case 'interstate': limit = 90;
-            break;
-        case 'city': limit = 50;
-            break;
-        case 'residential': limit = 20;
-            break;
-            default: break;
-    }
-    if(speed > limit + 20 && speed <= limit + 40) {
+    let limit = {
+        'motorway': 130,
+        'interstate': 90,
+        'city': 50,
+        'residential': 20
+    };
+    
+    if(speed > limit[area] + 20 && speed <= limit[area] + 40) {
         return "excessive speeding";
-    } else if (speed > limit && speed <= limit + 20) {
+    } else if (speed > limit[area] && speed <= limit[area] + 20) {
         return "speeding";
-    } else if (speed > limit + 40) {
+    } else if (speed > limit[area] + 40) {
         return "reckless driving";
     } else {
         return "";
