601cc31eeab8dc8ade5dd2064a6fbc265e322099
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-10-02 13:47:38 +0300

    Update 01.Print Array with Given Delimiter.js

--- 03.Arrays/01.Print Array with Given Delimiter.js
+++ 03.Arrays/01.Print Array with Given Delimiter.js
@@ -2,10 +2,13 @@ function solve(input) {
     return input.join(input.pop());
 }
 
-console.log(solve(['One', 
-'Two', 
-'Three', 
-'Four', 
-'Five', 
-'-']
+console.log(solve(
+    [
+        'One', 
+        'Two', 
+        'Three', 
+        'Four', 
+        'Five', 
+        '-'
+    ]
 ));
