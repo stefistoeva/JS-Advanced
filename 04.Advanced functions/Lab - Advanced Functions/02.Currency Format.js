5f88bcfbbc9567e6f0ebd09eb80bf96d05561db5
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-10-07 09:35:07 +0300

    Update 02.Currency Format.js

--- 04.Advanced functions/Lab - Advanced Functions/02.Currency Format.js
+++ 04.Advanced functions/Lab - Advanced Functions/02.Currency Format.js
@@ -2,7 +2,7 @@ function currencyFormatter(separator, symbol, symbolFirst, value) {
     let result = Math.trunc(value) + separator;
     result += value.toFixed(2).substr(-2, 2);
     if(symbolFirst) {
-        return Symbol + ' ' + result;
+        return symbol + ' ' + result;
     } else {
         return result + ' ' + symbol;
     }
