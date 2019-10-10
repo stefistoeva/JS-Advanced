9656f6f4dbe632ced931327845704d61a72fb3e6
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-10-10 09:07:53 +0300

    Add files via upload

---
+++ 05.DOM/01.Chat Room/solution.js
@@ -0,0 +1,20 @@
+function solve() {
+   const sendBtn = document.getElementById('send');
+   const messageContainer = document.getElementById('chat_input');
+
+   sendBtn.addEventListener('click', sendMessage);
+
+   function sendMessage() {
+      let message = messageContainer.value;
+      let newMessage = document.createElement('div');
+      newMessage.classList.add('message', 'my-message');
+      newMessage.textContent = message;
+
+      document.getElementById('chat_messages').appendChild(newMessage);
+
+      messageContainer.value = '';
+   }
+
+}
+
+
