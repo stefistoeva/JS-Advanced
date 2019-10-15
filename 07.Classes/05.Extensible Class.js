4d92a0bbebd5802a61c0f2cbb905a761bd4c1bad
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-10-15 08:37:13 +0300

    Create 05.Extensible Class.js

---
+++ 07.Classes/05.Extensible Class.js
@@ -0,0 +1,33 @@
+const Extensible = (function() {
+    let id = 0;
+
+    return class Extensible {
+        constructor() {
+            this.id = id++;
+        }
+
+        extend(template) {
+            Object.entries(template)
+                .forEach(([key, value]) => {
+                    if(typeof value === 'function') {
+                        Object.getPrototypeOf(this)[key] = value;
+                    } else {
+                        this[key] = value;
+                    }
+                })
+        }
+    }
+}())
+
+let obj1 = new Extensible();
+let obj2 = new Extensible();
+let obj3 = new Extensible();
+console.log(obj1.id);
+console.log(obj2.id);
+console.log(obj3.id);
+
+obj1.extend({
+   extensionMethod: function () { },
+   extensionProperty: 'someString'
+})
+console.log(Object.getPrototypeOf(obj1).extensionMethod);
