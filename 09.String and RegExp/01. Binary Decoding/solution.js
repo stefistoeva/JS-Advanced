cdc3733590c6807d7b19f8692f9b9b710e7f7617
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-10-20 20:38:22 +0300

    Create solution.js

---
+++ 09.String and RegExp/01. Binary Decoding/solution.js
@@ -0,0 +1,47 @@
+function solve() {
+	let input = document.getElementById('input').value;
+	let sum = calculateSum(input);
+	input = splitInput(input, sum);
+	let result = splitGroups(input);
+	showResult(result);
+
+	function showResult(result) {
+		document.getElementById('resultOutput').innerHTML = result;
+	}
+
+	function splitGroups(text) {
+		let matched = text.match(/.{1,8}/g);
+		let result = "";
+		matched.forEach(element => {
+			let asciiSymbol = String.fromCharCode(binaryToDecimal(element));
+			const regExp = /[A-z]+/gm;
+			if(regExp.exec(asciiSymbol)) {
+				result += asciiSymbol;
+			}
+		});
+		return result;
+	}
+
+	function binaryToDecimal(e) {
+		return parseInt(e, 2);
+	}
+
+	function splitInput(input, sum) {
+		let result = input.substring(sum);
+		result = result.substring(0, result.length - sum);
+		return result;
+	}
+
+	function calculateSum(input) {
+		while(input.toString().length > 1) {
+			input = spreadSum(input);
+		}
+		return input;
+	}
+
+	function spreadSum(num) {
+		return Array.from(num.toString())
+			.map(Number)
+			.reduce((acc, cur) => acc + cur);
+	}
+}
