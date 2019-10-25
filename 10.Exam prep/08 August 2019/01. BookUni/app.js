b340f3f91dc48532ba42a25f8949629ae42aa6a9
Stefi Stoeva <44346648+stefistoeva@users.noreply.github.com>
2019-10-25 09:09:34 +0300

    Update app.js

--- 10.Exam prep/08 August 2019/01. BookUni/app.js
+++ 10.Exam prep/08 August 2019/01. BookUni/app.js
@@ -1,57 +1,52 @@
 function solve() {
-    const [ titleInput, yearInput, priceInput ] = Array.from(document.querySelectorAll('input'));
-    const addBtn = document.querySelector('button');
-    const totalProfitHeader = Array.from(document.querySelectorAll('h1'))[1];
-    const [ oldShelf, newShelf ] = Array.from(document.getElementsByClassName('bookShelf'));
-    let totalSum = 0;
-
+    const [ titleIn, yearIn, priceIn ] = Array.from(document.getElementsByTagName('input'));
+    const addBtn = document.getElementsByTagName('button')[0];
+    const [ oldBooks, newBooks ] = Array.from(document.getElementsByClassName('bookShelf'));
+    const fieldForPrice = document.querySelector("body > h1:nth-child(3)");
+    let sum = 0;
+    
     addBtn.addEventListener('click', function(e) {
-        //when button is in a form !!! -> preventDefault();
+                //when button is in a form !!! -> preventDefault();
         e.preventDefault();
-        const title = titleInput.value;
-        const year = Number(yearInput.value);
-        const price = Number(priceInput.value);
-
-        if(title !== '' && year > 0 && price > 0) {
+        const title = titleIn.value;
+        const year = Number(yearIn.value);
+        const price = Number(priceIn.value);
+        if(title.value !== '' && year > 0 && price > 0) {
             if(year >= 2000) {
-                createBook(false, title, year, price, newShelf);
+                createBook(true, title, year, price, newBooks);
             } else {
-                createBook(true, title, year, price, oldShelf);
+                createBook(false, title, year, price, oldBooks);
             }
         }
     });
 
-    function createBook(isOld, title, year, price, shelf) {
-        price = isOld ? price * 0.85 : price;
-        const bookElement = document.createElement('div');
+    function createBook(isNew, bookTitle, bookYear, bookPrice, shelf) {
+        bookPrice = isNew === true ? bookPrice : 0.85 * bookPrice;
+        const div = document.createElement('div');
+        div.classList.add('book');
         const p = document.createElement('p');
+        p.textContent = `${bookTitle} [${bookYear}]`;
         const buyBtn = document.createElement('button');
-        
-        bookElement.classList.add('book');
-        p.textContent = `${title} [${year}]`;
-        buyBtn.textContent = `Buy it only for ${price.toFixed(2)} BGN`;
-
+        buyBtn.textContent = `Buy it only for ${bookPrice.toFixed(2)} BGN`;
         buyBtn.addEventListener('click', function(e) {
-            totalSum += price;
+            sum += bookPrice;
             this.parentNode.parentNode.removeChild(this.parentNode);
-
-        totalProfitHeader.textContent = `Total Store Profit: ${totalSum.toFixed(2)} BGN`;
+            fieldForPrice.textContent = `Total Store Profit: ${sum.toFixed(2)} BGN`;
         });
 
-        bookElement.appendChild(p); 
-        bookElement.appendChild(buyBtn);
-        if(!isOld) {
+        div.appendChild(p);
+        div.appendChild(buyBtn);
+
+        if(isNew) {
             const moveBtn = document.createElement('button');
-            moveBtn.textContent = `Move to old section`;
-
-            moveBtn.addEventListener('click', function() {
+            moveBtn.textContent = 'Move to old section';
+            moveBtn.addEventListener('click', function(e) {
                 this.parentNode.parentNode.removeChild(this.parentNode);
-                createBook(true, title, year, price, oldShelf);
-            });
-
-            bookElement.appendChild(moveBtn);
+                createBook(false, bookTitle, bookYear, bookPrice, oldBooks);
+            })
+            div.appendChild(moveBtn);
         }
 
-        shelf.appendChild(bookElement);
+        shelf.appendChild(div);
     }
 }
